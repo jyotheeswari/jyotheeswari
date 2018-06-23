e60cb098be84e4878ae590a61d2fc0a23c1b0060
admin <admin@DESKTOP-E45FOEL>
2018-06-23 03:02:22 -0700

    new project pushing into repository

---
+++ Maven/src/test/java/automation/Maven/SampleTest.java
@@ -0,0 +1,16 @@
+package automation.Maven;
+
+import org.testng.annotations.Test;
+import org.testng.annotations.BeforeTest;
+
+public class SampleTest<webdriver> {
+	 webdriver driver;
+  @Test
+  public void f() {
+	  System.out.println("jyothi");
+  }
+  @Test
+  public void g() {
+  }
+
+}
